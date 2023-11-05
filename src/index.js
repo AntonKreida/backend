f8df0bb6b640e418b13f0acc4a622a703d125f86
Kreida Anton <kreida.anton@yandex.ru>
2023-11-05 18:55:58 +0500

    homework-1 | add server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-const hello = () => {
-    console.log('hello, world!')
-}
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const url = new URL(req.url, 'http://127.0.0.1');
+    filePath = path.join(__dirname, './date', 'users.json');
+
+    if (url.searchParams.has('hello') && !url.searchParams.get('hello')) {
+        res.statusCode = 400;
+        res.end('Enter a name');
+        return;
+    }
+
+    if (url.searchParams.has('hello') && url.searchParams.get('hello')) {
+        res.statusCode = 200;
+        res.end(`Hello, ${url.searchParams.get('hello')}`);
+        return;
+    }
+
+    if(url.searchParams.has('users') && !url.searchParams.get('users')) {
+        req.headers['Content-Type'] = 'application/json';
+        fs.readFile(filePath, (error, data) => {
+            if(!error) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(data);
+                return;
+            }
+            res.statusCode = 500;
+            res.end('Server Error');
+        })
+        return;
+    }
+
+    if(!url.searchParams.has('users') || !url.searchParams.get('hello')) {
+        res.statusCode = 500;
+        res.end('Server Error');
+    }
+})
+
+server.listen(3003, () => {
+    console.log('Server running on port 3003')
+})
